94fb04f9236cb76eda5f517f0044a67f07bc1c41
bhushanrkashyap <bhushanrkashyap@gmail.com>
2025-08-21 23:05:11 +0530

    first commit

--- expensetracker/settings.py
+++ expensetracker/settings.py
@@ -75,10 +75,15 @@ WSGI_APPLICATION = 'expensetracker.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'expense',               
+        'USER': 'bhushanrkaashyap',                        
+        'PASSWORD': '',                 
+        'HOST': 'localhost',                     
+        'PORT': '5432',                   
     }
 }
+
 
 
 # Password validation
@@ -114,8 +119,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
+STATIC_URL = '/static/'
 
-STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
--- track/views.py
+++ track/views.py
@@ -1,3 +1,60 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.shortcuts import get_object_or_404, redirect
+from .models import *
+from django.contrib import messages
 
-# Create your views here.
+def index(request):
+    if request.method == "POST":
+        description = request.POST.get('description')
+        amount = request.POST.get('amount')
+
+        current_balance, _ = Balance.objects.get_or_create(id=1)
+        expense_type = "CREDIT"
+        if float(amount) < 0:
+            expense_type = "DEBIT"
+
+        if float(amount) == 0:
+            messages.success(request, "Amount cannot be zero")
+            return redirect('/')
+
+        tracking_history = TrackingHistory.objects.create(
+            amount=float(amount),
+            expense_type=expense_type,
+            current_balance=current_balance,
+            description=description
+        )
+        current_balance.current_balance += float(tracking_history.amount)
+        current_balance.save()
+
+        print(description, amount)
+        return redirect('/')
+
+    current_balance, _ = Balance.objects.get_or_create(id=1)
+
+    income = 0.0
+    expense = 0.0
+
+    for tracking_history in TrackingHistory.objects.all():
+        if tracking_history.expense_type == "CREDIT":
+            income += float(tracking_history.amount)
+        else:
+            expense += float(tracking_history.amount)
+
+    context = {
+        'income': income,
+        'expense': expense,
+        'transactions': TrackingHistory.objects.all(),
+        'current_balance': current_balance,
+    }
+    return render(request, 'index.html', context)
+def delete_transaction(request, transaction_id):
+    transaction = get_object_or_404(TrackingHistory, id=transaction_id)
+    current_balance = transaction.current_balance
+    if transaction.expense_type == 'CREDIT':
+        current_balance.current_balance -= transaction.amount
+    else:
+        current_balance.current_balance -= transaction.amount
+
+    current_balance.save()
+    transaction.delete()
+    return redirect('/')
