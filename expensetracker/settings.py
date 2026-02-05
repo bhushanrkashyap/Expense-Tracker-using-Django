d001418c40c6d8f29d9c9d67bd0da9e4d98444d3
bhushanrkashyap <bhushanrkashyap@gmail.com>
2026-02-05 21:14:23 +0530

    Database changes

--- expensetracker/settings.py
+++ expensetracker/settings.py
@@ -76,13 +76,14 @@ WSGI_APPLICATION = 'expensetracker.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'expense',               
-        'USER': 'bhushanrkaashyap',                        
-        'PASSWORD': '',                 
-        'HOST': 'localhost',                     
-        'PORT': '5432',                   
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres123',
+        'HOST': 'database-2.c41c4q0a4z2o.us-east-1.rds.amazonaws.com',
+        'PORT': '5432',
     }
 }
+
 
 
 
